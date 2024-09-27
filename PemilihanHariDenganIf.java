8d4646d9431741c3b193fd65109bd2b02208cb8c
nanda ricco <nandaricco2677@gmail.com>
2024-09-27 15:53:52 +0700

    hari

---
+++ PemilihanHariDenganIf.java
@@ -0,0 +1,26 @@
+package Praktikum04;
+import java.util.Scanner;
+
+public class PemilihanHariDenganIf {
+    
+    public static void main(String[] args) {
+        
+        Scanner hri = new Scanner(System.in);
+
+        int number;
+        String dayType;
+
+        System.out.print("Input number : ");
+        number = hri.nextInt();
+
+        if (number >=1 && number <=5 ){
+            dayType = "Weekday";
+            System.out.println("Hari ini adalah " + dayType);
+        } else if (number >=6 && number <= 7){
+            dayType = "Weekend";
+            System.out.println("Hari ini adalah " + dayType);
+        } else {
+            System.out.println("Invalid Number");
+        }
+    }
+}
